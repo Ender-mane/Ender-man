2985cdcbb9272e50966534bd2212b7aee89a471f
Ender-mane <joymoses2008@gmail.com>
2024-06-25 12:25:41 +0100

    First Commit

---
+++ ClassGenerics.java
@@ -0,0 +1,7 @@
+package course;
+  public class ClassGenerics {
+    public static void main(String[] args) {
+        int[] myNumbers = {1,2,3};
+        System.out.println(myNumbers[2]);
+    }
+  }
